dfd5a1c8a14c650097453158fc7c4b4315d1a2f0
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-03-13 17:45:41 +1000

    Add g1 real intro scripts

---
+++ scripts/inference/g1_real/sub_robot.py
@@ -0,0 +1,24 @@
+import numpy as np
+import rerun as rr
+from motion_tools.robot_gui import ReRunRobot
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
+
+from rs_imle_policy.unitree import G1_29_ArmController
+
+rec = rr.RecordingStream("g1_arm_controller_test")
+rec.spawn()
+
+
+robot_gui = ReRunRobot.g1(rec)
+
+# TODO: Add network interface as
+ChannelFactoryInitialize(networkInterface=os.environ["G1_NETWORK_INTERFACE"], id=0)  # dds domain id
+controller = G1_29_ArmController(motion_mode=False, simulation_mode=False, sub_mode=True)
+# breakpoint()
+
+while True:
+    q = controller.get_current_motor_q()
+    robot_gui.log(q)
+    # controller.ctrl_dual_arm(
+    #     np.random.rand(14), np.zeros(14)
+    # )  # Todo the tauff is the feedforward torque
