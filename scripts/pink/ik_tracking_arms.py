5d361b43b31b379ae077557682e9b3b85be4b04a
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-03-23 18:12:49 +1000

    add cyclonedds config

--- scripts/pink/ik_tracking_arms.py
+++ scripts/pink/ik_tracking_arms.py
@@ -1,6 +1,6 @@
 from loop_rate_limiters import RateLimiter
-from rs_imle_policy.g1_arm_ik import G1ReducedPinkIK
 
+from rs_imle_policy.g1_arm_ik import G1ReducedPinkIK
 
 ik = G1ReducedPinkIK(
     urdf_path="assets/g1.urdf",
@@ -8,6 +8,7 @@ ik = G1ReducedPinkIK(
     srdf_path="assets/g1.srdf",
     visualize=True,
     spawn_visualizer=True,
+    enable_self_collision=True,
 )
 
 targets = ik.get_targets_from_configuration()
@@ -16,6 +17,6 @@ ik.set_targets(targets.left, targets.right)
 rate = RateLimiter(200, warn=True)
 while True:
     rate.sleep()
-    q = ik.solve()
+    q = ik.solve(dt=1 / 200)
     ik.configuration.update(q.copy())
     ik.viz.display(q)
