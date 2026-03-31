94f18cf1e07c54b8ca4a4c68bd18b3b26707cc40
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-04-01 09:20:54 +1000

    Clean training script

--- scripts/train/g1_sim/train.py
+++ scripts/train/g1_sim/train.py
@@ -1,78 +1,72 @@
-import tyro
+from __future__ import annotations
+
+from typing import Annotated, TypeAlias
+
 import torch
+import tyro
 import wandb
+
+from rs_imle_policy.configs.default_configs import (
+    G1ArmsDiffusionConfig,
+    G1ArmsRSIMLEConfig,
+    G1LeftArmDiffusionConfig,
+    G1LeftArmRSIMLEConfig,
+    G1RightArmDiffusionConfig,
+    G1RightArmRSIMLEConfig,
+)
+from rs_imle_policy.configs.train_config import ExperimentConfig
 from rs_imle_policy.datasets import G1ArmsDataset
-from rs_imle_policy.configs.default_configs import G1ArmsRSIMLEConfig
-from rs_imle_policy.configs.train_config import G1VisionConfig
-from pathlib import Path
 from rs_imle_policy.policy import Policy
 from rs_imle_policy.train_real_world import train
 
+G1ExperimentConfigChoice: TypeAlias = (
+    Annotated[G1ArmsRSIMLEConfig, tyro.conf.subcommand(name="g1-arms-rsimle")]
+    | Annotated[G1ArmsDiffusionConfig, tyro.conf.subcommand(name="g1-arms-diffusion")]
+    | Annotated[G1LeftArmRSIMLEConfig, tyro.conf.subcommand(name="g1-left-arm-rsimle")]
+    | Annotated[G1LeftArmDiffusionConfig, tyro.conf.subcommand(name="g1-left-arm-diffusion")]
+    | Annotated[G1RightArmRSIMLEConfig, tyro.conf.subcommand(name="g1-right-arm-rsimle")]
+    | Annotated[G1RightArmDiffusionConfig, tyro.conf.subcommand(name="g1-right-arm-diffusion")]
+)
 
-def main(dataset_path: str, task_name: str, exp_name: str, debug: bool = False):
-    wandb.init(project=task_name)
 
-    # change wandb name
-    wandb.run.name = f"{exp_name}"
-    dataset = G1ArmsDataset(
-        Path(dataset_path),
-        pred_horizon=16,
-        obs_horizon=2,
-        action_horizon=8,
-        low_dim_obs_keys=[
-            "left_robot_pos",
-            "left_robot_orien",
-            "right_robot_pos",
-            "right_robot_orien",
-            "left_hand_state",
-            "right_hand_state",
-        ],
-        action_keys=[
-            "left_action_pos",
-            "left_action_orien",
-            "right_action_pos",
-            "right_action_orien",
-            "left_hand_action",
-            "right_hand_action",
-            "progress",
-        ],
-        vision_config=G1VisionConfig(),
-        use_next_state=True,
+def build_dataset(config: ExperimentConfig) -> G1ArmsDataset:
+    return G1ArmsDataset(
+        config.dataset_path,
+        pred_horizon=config.model.pred_horizon,
+        obs_horizon=config.model.obs_horizon,
+        action_horizon=config.model.action_horizon,
+        low_dim_obs_keys=config.data.lowdim_obs_keys,
+        action_keys=config.data.action_keys,
+        vision_config=config.data.vision,
+        use_next_state=config.data.use_next_state,
     )
 
-    if debug:
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=64,
-            num_workers=0,
-            shuffle=True,
-            pin_memory=True,
-            persistent_workers=False,
-        )
-        print("debug")
-    else:
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=64,
-            num_workers=11,
-            shuffle=True,
-            pin_memory=True,
-            persistent_workers=True,
-        )
-        print("no debug")
 
-    config = G1ArmsRSIMLEConfig(
-        exp_name=exp_name,
-        dataset_path=Path(dataset_path),
-        task_name=task_name,
-        debug=debug,
+def build_dataloader(config: ExperimentConfig, dataset: G1ArmsDataset) -> torch.utils.data.DataLoader:
+    num_workers = 0 if config.debug else config.training_params.num_workers
+    persistent_workers = num_workers > 0
+
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=config.training_params.batch_size,
+        num_workers=num_workers,
+        shuffle=True,
+        pin_memory=True,
+        persistent_workers=persistent_workers,
     )
+
+
+def main(config: G1ExperimentConfigChoice) -> None:
+    wandb.init(project=config.task_name)
+    wandb.run.name = f"{config.exp_name}_{config.model.name}"
+
+    dataset = build_dataset(config)
+    dataloader = build_dataloader(config, dataset)
+
     policy = Policy(config=config, dataset=dataset)
-    nets = policy.nets
-
     train(
         config,
-        nets,
+        policy.nets,
         dataloader,
         policy.noise_scheduler,
         policy.optimizer,
@@ -84,4 +78,4 @@ def main(dataset_path: str, task_name: str, exp_name: str, debug: bool = False):
 
 
 if __name__ == "__main__":
-    tyro.cli(main)
+    main(tyro.cli(G1ExperimentConfigChoice))
