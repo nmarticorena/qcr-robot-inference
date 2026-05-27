315efe80d92479c5925a02475b2d851bc459034a
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-05-27 13:24:37 +1000

    add config for evaluation scripts

---
+++ rs_imle_policy/configs/franka/evaluation.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from rs_imle_policy.configs.train_config import VisionConfig
+
+
+@dataclass
+class EvaluationConfig:
+    task_name: str = "example"
+    output_dir: Path = Path("experiments/franka_evaluation")
+    robot_ip: str = "172.16.0.2"
+    n_experiments: int = 10
+    vision: VisionConfig = field(default_factory=VisionConfig)
