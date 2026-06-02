c0e4c4d93c3f3cd2780e15d627a863c0ffa8e652
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-06-02 13:06:25 +1000

    expose unnormalize data

--- rs_imle_policy/datasets/__init__.py
+++ rs_imle_policy/datasets/__init__.py
@@ -1,7 +1,7 @@
 """Dataset abstractions and concrete implementations."""
 
-from rs_imle_policy.datasets.base_dataset import BaseDataset
+from rs_imle_policy.datasets.base_dataset import BaseDataset, unnormalize_data
 from rs_imle_policy.datasets.single_franka import PandaPolicyDataset
 from rs_imle_policy.datasets.g1_arms import G1ArmsDataset
 
-__all__ = ["BaseDataset", "PandaPolicyDataset", "G1ArmsDataset"]
+__all__ = ["BaseDataset", "PandaPolicyDataset", "G1ArmsDataset", "unnormalize_data"]
