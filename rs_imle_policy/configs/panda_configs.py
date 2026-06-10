2a9659481aae0f50c40bac6de9d61f9de21e4ff4
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-06-10 15:05:25 +1000

    typing fix

--- rs_imle_policy/configs/panda_configs.py
+++ rs_imle_policy/configs/panda_configs.py
@@ -1,23 +1,25 @@
 from typing import List
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import numpy as np
 
 @dataclass
 class SinglePandaConfig:
     """
-    Config for frankx controller to keep the same args over all experiments
+    Config for frankx/pandanpy controller to keep the same args over all experiments
     """
     robot_ip:str = "172.16.0.2"
 
     # Controller
-    cartesian_impedance:List[float] = [400.0, 400.0, 400.0, 40.0, 40.0, 40.0] # 
+    cartesian_impedance:List[float] = field(
+        default_factory=lambda: [400.0, 400.0, 400.0, 40.0, 40.0, 40.0]
+    )
     dynamic_rel:float = 0.2
     accel_rel:float = 0.1
     jerk_rel:float = 0.01
     repeat_on_error: bool = True
 
     # Stop behaviour
-    stop_duration_s: float = 0.1
+    stop_duration_s: float = 1.
     stop_rate_hz: float = 10
     
     # Gripper
@@ -26,12 +28,21 @@ class SinglePandaConfig:
     gripper_force: float = 40. # [Nm]
 
     # Task specific
-    default_q = [0.0, -np.pi / 4, 0.0, -3 * np.pi / 4, 0.0, np.pi / 2, np.pi / 4]
+    default_q: List[float] = field(
+        default_factory=lambda: [
+            0.0,
+            -np.pi / 4,
+            0.0,
+            -3 * np.pi / 4,
+            0.0,
+            np.pi / 2,
+            np.pi / 4,
+        ]
+    )
 
     # Policy related
     action_hz:float = 10. # Desire frecuency between waypoints
     gripper_close_th:float = 0.5 # th for consider the binnary action of closing is executed 
     progress_complete_th:float = 0.9 # th for consider the progress action is consider as successful
-
 
 
