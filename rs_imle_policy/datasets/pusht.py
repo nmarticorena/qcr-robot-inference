d6345a8f9713fa701ed7b70b4aad0afd6860cbb3
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-06-15 17:50:06 +1000

    clean code

--- rs_imle_policy/datasets/pusht.py
+++ rs_imle_policy/datasets/pusht.py
@@ -1,34 +1,37 @@
 
-import time
 import pickle
-import numpy as np
-from numpy.typing import NDArray
+import time
 from pathlib import Path
 
-from rs_imle_policy.datasets.base_dataset import BaseDataset
+import numpy as np
+from numpy.typing import NDArray
+
 from rs_imle_policy.configs.train_config import PushTVisionConfig
+from rs_imle_policy.datasets.base_dataset import BaseDataset
 
 
 class PushTDataset(BaseDataset):
     def __init__(self, *args, **kwargs):
         vision_config = PushTVisionConfig()
-        super().__init__(load_images = False, 
-                         vision_config = vision_config, 
-                         skip_normalization_keys=("images",),
-                         *args, 
-                         **kwargs)
+        super().__init__(
+            load_images=False,
+            vision_config=vision_config,
+            skip_normalization_keys=("images",),
+            *args,
+            **kwargs,
+        )
 
     def create_rlds_dataset(self) -> dict[int, dict[str, NDArray]]:
-        with open(self.dataset_path / "pusht.pkl", 'rb') as f:
+        with open(self.dataset_path / "pusht.pkl", "rb") as f:
             rlds = pickle.load(f)
         # create a new entry to rlds for each episode to store position data which is a slice of the obs
         for episode in rlds.keys():
             pos_array = []
-            for i in range(len(rlds[episode]['obs'])):
-                pos_array.append(rlds[episode]['obs'][i][:2])
-            
-            rlds[episode]['state'] = pos_array
-        rlds = self.add_padding(rlds, pad_before=self.obs_horizon-1, pad_after=self.pred_horizon)
+            for i in range(len(rlds[episode]["obs"])):
+                pos_array.append(rlds[episode]["obs"][i][:2])
+
+            rlds[episode]["state"] = pos_array
+        rlds = self.add_padding(rlds, pad_before=self.obs_horizon - 1, pad_after=self.pred_horizon)
         return rlds
 
     def add_padding(self, rlds, pad_before, pad_after):
@@ -37,42 +40,57 @@ class PushTDataset(BaseDataset):
 
         for episode in rlds.keys():
             # Extract episode data
-            state = rlds[episode]['state']
-            action = rlds[episode]['action']
-            images = rlds[episode]['images']
-            
+            state = rlds[episode]["state"]
+            action = rlds[episode]["action"]
+            images = rlds[episode]["images"]
+
             # Pre-padding
             pre_pad_pos = [state[0]] * pad_before
             pre_pad_action = [action[0]] * pad_before
             pre_pad_images = np.repeat(images[:1], pad_before, axis=0)
 
-            
             # Post-padding
             post_pad_pos = [state[-1]] * pad_after
             post_pad_action = [action[-1]] * pad_after
             post_pad_images = np.repeat(images[-1:], pad_after, axis=0)
-            
+
             # Apply padding
-            rlds[episode]['state'] = np.array(pre_pad_pos + state + post_pad_pos)
-            rlds[episode]['action'] = np.array(pre_pad_action + action.tolist() + post_pad_action)
-            rlds[episode]['images'] = np.concatenate([pre_pad_images, images, post_pad_images], axis=0)
-        
+            rlds[episode]["state"] = np.array(pre_pad_pos + state + post_pad_pos)
+            rlds[episode]["action"] = np.array(pre_pad_action + action.tolist() + post_pad_action)
+            rlds[episode]["images"] = np.concatenate([pre_pad_images, images, post_pad_images], axis=0)
+
         return rlds
 
+    def sample_sequence(self, episode: int, buffer_start_idx: int, buffer_end_idx: int) -> dict:
+        seq = super().sample_sequence(episode, buffer_start_idx, buffer_end_idx)
+        seq["frames"]["frames"] = self.format_images(seq["frames"]["frames"])
+        return seq
 
-    
+    @staticmethod
+    def format_images(images: NDArray) -> NDArray:
+        images = np.asarray(images)
+        if images.ndim != 4:
+            raise ValueError(f"Expected PushT images with shape T,H,W,C or T,C,H,W, got {images.shape}")
+        if images.shape[-1] in (1, 3, 4):
+            images = np.moveaxis(images, -1, 1)
+        if images.shape[1] == 4:
+            images = images[:, :3]
+        images = images.astype(np.float32)
+        if images.max() > 1.0:
+            images = images / 255.0
+        return images
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dataset = PushTDataset(
-                 dataset_path=Path('data/pusht'),
-                 pred_horizon=16,
-                 obs_horizon=2,
-                 action_horizon=2,
-                )
-                 # dataset_percentage=1.0)
-    
-    idx=0
+        dataset_path=Path("data/pusht"),
+        pred_horizon=16,
+        obs_horizon=2,
+        action_horizon=2,
+    )
+    # dataset_percentage=1.0)
+
+    idx = 0
     while True:
         start_time = time.time()
         dataset.__getitem__(idx)
