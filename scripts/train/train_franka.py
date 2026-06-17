08004d8ff12db62a776b31b0d082666efeb4a009
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-06-17 14:23:36 +1000

    remove breakpoint

--- scripts/train/train_franka.py
+++ scripts/train/train_franka.py
@@ -8,7 +8,6 @@ from rs_imle_policy.train import run_training
 
 def main(config: FrankaExperimentConfigChoice) -> None:
     train_dataset, val_dataset = PandaPolicyDataset.train_val(config, val_episode_count=10)
-    breakpoint()
     run_training(config, train_dataset, val_dataset=val_dataset)
 
 
