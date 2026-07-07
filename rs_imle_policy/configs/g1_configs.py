948f74fcc28616f51c7a540db60a29efab18a011
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-07-07 13:32:22 +1000

    tune controller for sim

--- rs_imle_policy/configs/g1_configs.py
+++ rs_imle_policy/configs/g1_configs.py
@@ -70,6 +70,11 @@ class G1IKConfig:
 class G1IKConfigSim(G1IKConfig):
     urdf_path: str = "assets/g1_no_hands.urdf"
     srdf_path: str = "assets/g1_29dof_rev_1_0_with_inspire_hand_FTP.srdf"
+    damping_cost: float = 1.
+    acceleration_limit: Optional[float] = 100.
+
+    ee_offset: float = 0.0
+    # box_barrier_gain: float = 1.0
 
 
 @dataclass
