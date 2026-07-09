b14216df62a224e5c0edf69acee50a5601788966
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-07-09 14:11:58 +1000

    first working version, need to compute a better error

--- rs_imle_policy/controllers/g1/base_controller.py
+++ rs_imle_policy/controllers/g1/base_controller.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 import time
+import numpy as np
 
 import spatialmath as sm
 import spatialmath.base as smb
+from loop_rate_limiters import RateLimiter
 
 from unitree_sdk2py.core.channel import ChannelPublisher, ChannelSubscriber, ChannelFactoryInitialize
 from unitree_sdk2py.utils.crc import CRC
@@ -34,67 +36,120 @@ class Pose:
         return [self.x, self.y, self.z], [self.rx, self.ry, self.rz, self.w]
 
 
-class G1BaseController():
-
+class G1BaseController:
     def __init__(
         self,
-        target_pose_topic = "rt/dolly_pose",
-        robot_pose_topic = "rt/torso_pose",
-        robot_state_topic = "rt/sportmodestate",
-        base_command = "rt/cmd_vel",
+        hz: int = 50,
+        target_pose_topic="rt/dolly_pose",
+        robot_pose_topic="rt/torso_pose",
+        robot_state_topic="rt/sportmodestate",
+        base_command="rt/run_command/cmd",
     ):
-        self.target_vel = [0.0,0.0,0.0] # [vx,vy,omega]
+        self.hz = hz
+        self.target_vel = [0.0, 0.0, 0.0]
+
+        self.x_wrp = Pose()
+        self.x_wr = Pose()
+        self.current_vel = [0.0, 0.0, 0.0]
+
+        self.target_pose_first_frame = False
+        self.robot_pose_first_frame = False
+        self.robot_state_first_frame = False
+
         self.command_publisher = ChannelPublisher(base_command, String_)
         self.command_publisher.Init()
 
-        ## Subcribers
         self.target_pose_sub = ChannelSubscriber(target_pose_topic, PoseStamped_)
-        self.target_pose_first_frame = False
-        self.target_pose = Pose()
-        self.target_pose_sub.Init(lambda x: self.pose_callback(x, self.target_pose_first_frame, self.target_pose), 1)
+        self.target_pose_sub.Init(self.target_pose_callback, 1)
 
         self.robot_pose_sub = ChannelSubscriber(robot_pose_topic, PoseStamped_)
-        self.robot_pose_first_frame = False
-        self.robot_pose = Pose()
-        self.robot_pose_sub.Init(lambda x:self.pose_callback(x, self.robot_pose_first_frame, self.robot_pose), 1)
-        
+        self.robot_pose_sub.Init(self.robot_pose_callback, 1)
 
-        # self.robot_state_sub = ChannelSubscriber(robot_state_topic, SportModeState_)
-        # self.robot_state_sub.Init(self.robot_state_callback, 1)
-        self.robot_state_first_frame = False
+        self.robot_state_sub = ChannelSubscriber(robot_state_topic, SportModeState_)
+        self.robot_state_sub.Init(self.robot_state_callback, 1)
 
-
-        while (not self.robot_state_first_frame) or (not self.target_pose_first_frame) or (not self.robot_pose_first_frame):
+        while not self.ready():
             print("Waiting for topics")
-            print(self.target_pose)
-            print(self.robot_pose)
+            # print("target ready:", self.target_pose_first_frame, self.target_pose)
+            # print("robot ready:", self.robot_pose_first_frame, self.robot_pose)
+            # print("state ready:", self.robot_state_first_frame, self.current_vel)
             time.sleep(1)
 
-    def pose_callback(self, msg: PoseStamped_, first_frame_flag, pose: Pose):
+        print("All topics received. Starting controller.")
+
+        rate = RateLimiter(hz)
+        while True:
+            self.step()
+            rate.sleep()
+
+    def ready(self):
+        return (
+            self.robot_state_first_frame
+            and self.target_pose_first_frame
+            and self.robot_pose_first_frame
+        )
+
+    def robot_state_callback(self, msg: SportModeState_):
+        self.current_vel = msg.velocity
+        self.robot_state_first_frame = True
+
+    def target_pose_callback(self, msg: PoseStamped_):
+        self.update_pose_from_msg(msg, self.x_wrp)
+        self.target_pose_first_frame = True
+
+    def robot_pose_callback(self, msg: PoseStamped_):
+        self.update_pose_from_msg(msg, self.x_wr)
+        self.robot_pose_first_frame = True
+
+    def update_pose_from_msg(self, msg: PoseStamped_, pose: Pose):
         pos = msg.pose.position
         quat = msg.pose.orientation
-
 
         pose.x = pos.x
         pose.y = pos.y
         pose.z = pos.z
+
         pose.w = quat.w
         pose.rx = quat.x
         pose.ry = quat.y
         pose.rz = quat.z
 
-        if not first_frame_flag:
-            first_frame_flag = True
+    def publish_command(self, vx,vy,omega):
+        dt = 1.0 / self.hz
 
-    # def callback_target_pose(self, msg: PoseStamped_):
-    #     pos = msg.pose.position
-    #     quat = msg.pose.orientation
-    #
-    #
-    #
-    #     if not self.target_pose_first_frame:
-    #         self.target_pose_first_frame = True
+        target = np.array([vx, vy, omega], dtype=float)
 
+        if not hasattr(self, "current_vel"):
+            self.current_vel = np.zeros(3)
+
+        max_acc = np.array([
+            300,  # max vx acceleration [m/s^2]
+            200,  # max vy acceleration [m/s^2]
+            100.,  # max omega acceleration [rad/s^2]
+        ])
+
+        delta = target - self.current_vel
+        max_delta = max_acc * dt
+
+        delta = np.clip(delta, -max_delta, max_delta)
+
+        self.current_vel = self.current_vel + delta
+
+        data = f"{self.current_vel[-1]} {self.current_vel[1]} {self.current_vel[2]} 0.8"
+        cmd = String_(data)
+        self.command_publisher.Write(cmd)
+
+    def step(self):
+        # compute and publish command here
+        x_rrp:sm.SE3 = self.x_wr.to_se3().inv() * self.x_wrp.to_se3() # Local pose
+
+        vx, vy = x_rrp.t[:2] * 0.5
+        rpy = x_rrp.rpy(order ="zyx")
+        omega = rpy[0] * 10
+        self.publish_command(vx,vy,omega)
+
+
+    
 
 if __name__ == "__main__":
     ChannelFactoryInitialize(id = 1, networkInterface="lo")
