c2cb18cb74ca585a770ee52e646684a704a9a2cd
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-07-21 08:33:38 +1000

    make channels to have a default

--- rs_imle_policy/utils/dds/commands.py
+++ rs_imle_policy/utils/dds/commands.py
@@ -6,7 +6,7 @@ from rs_imle_policy.utils.dds.publisher import publish_string
 RopeCommand = Literal["enable", "disable"]
 
 
-def reset_sim(channel: str) -> None:
+def reset_sim(channel: str = "rt/reset_category") -> None:
     """Reset the simulation through the configured DDS channel."""
     publish_string(channel, "-1")
 
@@ -16,6 +16,6 @@ def reset_dolly(channel: str="rt/reset_dolly") -> None:
     publish_string(channel, "reset")
 
 
-def set_rope(channel: str, command: RopeCommand) -> None:
+def set_rope(command: RopeCommand, channel: str = "rt/elastic_band") -> None:
     """Enable or disable the rope through the configured DDS channel."""
     publish_string(channel, command)
